1b615d83dedbe9d3b089b39d2470f027abd424cf
Imanbeq <imanqlove@gmail.com>
2022-02-24 20:04:14 +0600

    feat: added js code

---
+++ 22.02/script.js
@@ -0,0 +1,17 @@
+let firstNumber = 1;
+let second_number = 2;
+let ThirdNumber = 3;
+alert(firstNumber);
+alert(second_number);
+alert(ThirdNumber);
+
+let str1 = 'Salam, ';
+let str2 = 'Bishkek!';
+alert(str1 + str2);
+
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+alert(result);
