be0f26428d37c7a9355e0e4eadf29c3b90c1d09d
Imanbeq <imanqlove@gmail.com>
2022-03-01 18:35:55 +0600

    added: style and js code

---
+++ 24.02/script.js
@@ -0,0 +1,89 @@
+/*
+let num = 10;
+num++;
+num++;
+num--;
+console.log(num);
+
+let num1 = 47;
+num1 += 7;
+num1 -= 18;
+num1 *= 10;
+num1 /= 15;
+console.log(num1);
+
+*/
+
+/****** && ******/
+
+/*
+const name = 'Iman';
+const age = 26;
+const result = age == 26 && name == 'Iman';
+console.log(result); // true
+*/
+
+/*
+const name = 'Iman';
+const age = 26;
+const result = age == 26 && name == 'Dima';
+console.log(result); // false
+*/
+
+/*
+const name = 'Iman';
+const age = 26;
+const result = age == 28 && name == 'Iman';
+console.log(result); // false
+*/
+
+/*
+const name = 'Iman';
+const age = 26;
+const result = age == 28 && name == 'Dima';
+console.log(result); // false
+*/
+
+
+/****** || ******/
+
+/*
+const day = 10;
+const night = 14;
+const sytki = day == 10 || night == 14;
+console.log(sytki); // true
+*/
+
+/*
+const day = 10;
+const night = 14;
+const sytki = day == 10 || night == 15;
+console.log(sytki); // true
+*/
+
+/*
+const day = 10;
+const night = 14;
+const sytki = day == 11 || night == 14;
+console.log(sytki); // true
+*/
+
+/*
+const day = 10;
+const night = 14;
+const sytki = day == 11 || night == 15;
+console.log(sytki); // false
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
