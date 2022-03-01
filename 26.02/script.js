43822a1b51e0d9762106ecaea78299565d8480cf
Imanbeq <imanqlove@gmail.com>
2022-03-01 19:13:24 +0600

    added: bg-color and js code

---
+++ 26.02/script.js
@@ -0,0 +1,48 @@
+/*
+let x = 7;
+if (x){
+    alert('Вы правы!');
+}else{
+    alert('Вы неправы!');
+}
+*/
+
+/*
+let a = 1;
+let b = 3;
+if( a >= 1, b >= 3 ){
+    alert( a + b);
+}else{
+    alert( a - b);
+}
+*/
+
+/*
+let time = 4;
+if(time >= 0 && time <= 14){
+    alert('Вы в первой четверти!');
+}else if(time >= 15 && time <= 30){
+    alert('Вы во второй четверти!');
+}else if(time >= 31 && time <= 45){
+    alert('Вы в третьей четверти!');
+}else if(time >= 46 && time <= 59){
+    alert('Вы в четвертой четверти!');
+}else{
+    alert('Значение не попадает в диапазон от 0 до 59');
+}
+*/
+
+/*
+let test = true;
+if(test){
+    alert('Верно!')
+}else{
+    alert('Неверно')
+}
+*/
+
+/*
+let test = false;
+let result = test == true ? 'Верно!' : 'Неверно!';
+alert(result);
+*/
