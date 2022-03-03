0faed7f3c5cb26af60b3f6ff83eeb3002812dc1b
Imanbeq <imanqlove@gmail.com>
2022-03-03 18:01:58 +0600

    added: js code

---
+++ 01.02/script.js
@@ -0,0 +1,17 @@
+const userSalaries = {
+    Ella: 1000,
+    Sophia: 1200,
+    Ellie: 900
+}
+console.log(userSalaries.Ella, userSalaries.Sophia);
+
+const obj = {
+    a: 5,
+    b: 6,
+    c: 10
+}
+console.log( obj.a + obj.b + obj.c);
+
+let number = false;
+const result = number === true ? 3 : 5;
+console.log(result);
