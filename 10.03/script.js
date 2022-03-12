de48b3e8dc4d0e91b9e12519fed10405c048552c
Imanbeq <imanqlove@gmail.com>
2022-03-12 17:08:29 +0600

    added: js code

--- 10.03/script.js
+++ 10.03/script.js
@@ -1,7 +1,7 @@
+/*
 let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22];
 let arrEven = [];
 let arrNotEven = [];
-let newobj = {};
 for(let i = 0; i < arr.length; i++){
     if(arr[i] % 2 == 0){
         arrEven.push(arr[i]);
@@ -13,3 +13,44 @@ for(let i = 0; i < arr.length; i++){
 
 console.log(arrEven);
 console.log(arrNotEven);
+*/
+
+/*
+function minNum(a, b) {
+    return( a < b )? a : b;
+}
+
+function showNum(x) {
+    console.log(x);
+}
+
+let result = minNum(86, 66);
+showNum(result);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
