df4f0496c9b6750aff3e9b529c3e5209e61248d2
Imanbeq <imanqlove@gmail.com>
2022-03-22 17:41:21 +0600

    added: js code

---
+++ 19.03/scripts.js
@@ -0,0 +1,23 @@
+/*
+const showMessage = (delay) => {
+    alert('Hello after ' + delay + ' seconds!');
+}
+setTimeout(showMessage, 4000, 4);
+setTimeout(showMessage, 8000, 8);
+*/
+
+/*
+const mul = (n,m) => {
+    return n * m;
+}
+console.log(mul(5,5));
+*/
+
+
+/*
+function factorial(n) {
+    return (n != 1) ? n * factorial(n - 1) : 1;
+}
+
+alert( factorial(7) );
+*/
