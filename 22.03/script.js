c3f821297ad32d06032436998019fad722cdec07
Imanbeq <imanqlove@gmail.com>
2022-03-24 18:06:46 +0600

    added: js code

---
+++ 22.03/script.js
@@ -0,0 +1,25 @@
+
+/*
+let inputNum = prompt('Введите число');
+let x = 0;
+while (y != 99) {
+    x++;
+    inputNum = prompt('Введите число');
+}
+alert('Количество чисел - ' + x) ;
+*/
+
+/*
+let inputNum = parseFloat(prompt('Введите число'));
+let x = 0;
+let sum = 0;
+while (inputNum != 999) {
+    x++;
+    sum += inputNum;
+    inputNum = parseFloat(prompt('Введите число'));
+}
+alert('Количество чисел - ' + (x));
+alert(('Сумма = ') + (sum));
+*/
+
+
