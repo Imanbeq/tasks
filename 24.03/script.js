6a28b6c157d2c7a398e7beb59d688a6540d8ee7b
Imanbeq <imanqlove@gmail.com>
2022-03-31 13:38:12 +0600

    added: html structure and js code

---
+++ 24.03/script.js
@@ -0,0 +1,70 @@
+/*
+const giveNumber = (x) => {
+    let inputNum = prompt('Введите число');
+    while (inputNum != 99) {
+        x++;
+        inputNum = prompt('Введите число');
+    }
+    return ('Количество чисел - ') +  x;
+}
+
+const sumNumbers = (y, sum) => {
+    let inputNum1 = parseFloat(prompt('Введите число'));
+    while (inputNum1 != 999) {
+        y++;
+        sum += inputNum1;
+        inputNum1 = parseFloat(prompt('Введите число'));
+    }
+    return ('Количество чисел - ') + y + (' сумма - ') + sum;
+
+}
+
+const showResult = (text) => {
+    console.log(text);
+}
+showResult(giveNumber(0));
+showResult(sumNumbers(0, 0));
+*/
+
+/*
+const showHowOld = () => {
+    let age = parseFloat(prompt('Введите ваш год рождения'));
+    let year = parseFloat(prompt('Введите нынешний год'));
+    return `Вам ${age - year} лет`;
+}
+alert(showHowOld());
+*/
+
+/*
+const showMessage = (text) => {
+    let x = 0;
+    const intervalId = setInterval(() => {
+        console.log(text);
+        x++;
+        if(x === 5){
+            clearInterval(intervalId);
+            console.log('Done');
+        }
+    }, 1000)
+}
+showMessage('Hello world');
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
