35771a69a6059c3784cd88ac32dc17208e09960c
Qlove Iman <imanqlove@gamil.com>
2022-04-16 14:15:25 +0600

    added: html and js code

---
+++ 07.04/script.js
@@ -0,0 +1,34 @@
+const btn = document.querySelector('#btn');
+
+
+btn.onclick = (e) => {
+    e.preventDefault();
+    const elems = document.querySelectorAll('.elems');
+    let x = 0;
+    let data = {};
+    for(let i = 0; i < elems.length; i++){
+        data[elems[i].name] = elems[i].value;
+        if(elems[i].value === ''){
+            x++;
+        }
+    }
+    if(x > 0){
+        alert('Заполните все поля!');
+    } else{
+        console.log(data);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
